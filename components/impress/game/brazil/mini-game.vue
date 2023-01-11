02e9c7d609718074645b37fa81455302feab345d
oelleuch1 <102473851+oelleuch1@users.noreply.github.com>
2023-01-11 17:18:57 +0100

    fix mini game (#7)

--- components/impress/game/brazil/mini-game.vue
+++ components/impress/game/brazil/mini-game.vue
@@ -1,29 +1,52 @@
 <template>
-  <v-card light tile flat class="card pt-16">
-    <v-card-text class="pa-0 card__content">
-      <div class="card__content__main">
+  <v-card class="card transparent pt-16" flat light tile>
+    <v-img
+      :src="require('~/assets/images/games/brazil/cameras_background.png')"
+      class="d-flex align-center justify-center"
+    >
+      <div class="card__infos d-flex justify-space-between">
         <div
-          v-for="({ moles }, lineIdx) in lines"
-          :key="lineIdx"
-          class="card__content__main__line"
+          class="white--text text-center rounded-lg font-weight-bold pa-2 card__infos__item"
         >
-          <img
-            v-for="(mole, moleIdx) in moles"
-            :key="moleIdx"
-            class="image mx-16"
-            :src="
-              require(`~/assets/images/games/brazil/mole-${mole.status}.png`)
-            "
-            @click="hitMoleHandler(mole)"
-          />
+          {{ $t('brazil.mini_game.high_score') }} {{ getHighestScore }}
+        </div>
+        <div class="d-flex">
+          <div
+            class="white--text text-center rounded-lg font-weight-bold pa-2 mr-4 card__infos__item"
+          >
+            {{ score }} {{ $t('brazil.mini_game.points') }}
+          </div>
+          <div
+            class="white--text text-center rounded-lg font-weight-bold pa-2 card__infos__item"
+          >
+            {{ timeRemainingSecs }}
+            {{ $t('brazil.mini_game.seconds') }}
+          </div>
         </div>
       </div>
-    </v-card-text>
+      <v-card-text class="pa-0 card__content">
+        <div
+          v-for="({ cameras }, lineIdx) in lines"
+          :key="lineIdx"
+          class="card__content__line"
+        >
+          <img
+            v-for="(camera, camIdx) in cameras"
+            :key="camIdx"
+            :src="
+              require(`~/assets/images/games/brazil/camera_${camera.status}.png`)
+            "
+            class="image mx-4"
+            @click="hitCameraHandler(camera)"
+          />
+        </div>
+      </v-card-text>
+    </v-img>
   </v-card>
 </template>
 
 <script>
-const MOLE_STATUS = {
+const CAMERA_STATUS = {
   VISIBLE: 'visible',
   HIDDEN: 'hidden',
   HIT: 'hit',
@@ -35,31 +58,48 @@ export default {
     return {
       lines: [
         {
-          moles: [{ status: MOLE_STATUS.HIDDEN }],
-        },
-        {
-          moles: [
-            { status: MOLE_STATUS.HIDDEN },
-            { status: MOLE_STATUS.HIDDEN },
+          cameras: [
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
           ],
         },
         {
-          moles: [
-            { status: MOLE_STATUS.HIDDEN },
-            { status: MOLE_STATUS.HIDDEN },
-            { status: MOLE_STATUS.HIDDEN },
+          cameras: [
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
+          ],
+        },
+        {
+          cameras: [
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
+            {status: CAMERA_STATUS.HIDDEN},
           ],
         },
       ],
+      timeRemainingSecs: 30,
       restartCount: 0,
       score: 0,
-      scores: []
+      scores: [],
     };
   },
   computed: {
     canRestart() {
-      return this.restartCount < 5
+      return this.restartCount < 4;
     },
+    getHighestScore() {
+      return this.scores.length ? Math.max(...this.scores) : 0;
+    },
+  },
+  watch: {
+    timeRemainingSecs(value) {
+      if (!value) {
+        this.scores.push(this.score)
+      }
+    }
   },
   mounted() {
     this.$store.commit('SET_INSTRUCTIONS', {
@@ -72,72 +112,69 @@ export default {
   },
   methods: {
     startGame() {
-      const intervalRef = setInterval(this.gameManager, 1000);
+      const gameIntervalRef = setInterval(this.gameManager, 1000);
+      const timeIntervalRef = setInterval(() => this.timeRemainingSecs--, 1000);
 
       setTimeout(() => {
-        clearInterval(intervalRef);
+        clearInterval(gameIntervalRef);
+        clearInterval(timeIntervalRef);
+
         this.$store.commit('SET_INSTRUCTIONS', {
           model: true,
           title: this.$t('brazil.mini_game.title'),
-          steps: [this.$t('brazil.mini_game.score', { score: this.score })],
+          steps: [this.$t('brazil.mini_game.score', {score: this.getHighestScore})],
           nextText: this.$t('finish'),
           nextMethod: this.finishGame,
           cancelable: this.canRestart,
           cancelText: this.$t('restart'),
-          cancelMethod: this.restartGame
+          cancelMethod: this.restartGame,
         });
-      }, 30 * 1000);
+      }, this.timeRemainingSecs * 1000);
     },
     gameManager() {
       const randomLine = Math.floor(Math.random() * 3);
-      const randomMole = Math.floor(Math.random() * 3);
+      const randomCamera = Math.floor(Math.random() * 3);
       const randomHide = Math.random() * 3000;
       const randomHideSecs = randomHide >= 1000 ? randomHide : 1000;
 
       if (randomLine <= 2) {
-        const { moles } = this.lines[randomLine];
-        const selectedMole =
-          moles[randomMole] ||
-          moles[randomLine] ||
-          moles[randomMole - 1] ||
-          moles[randomLine - 1] ||
-          moles[0];
-        selectedMole.status = MOLE_STATUS.VISIBLE;
+        const {cameras} = this.lines[randomLine];
+        const selectedCamera = cameras[randomCamera];
+        selectedCamera.status = CAMERA_STATUS.VISIBLE;
         setTimeout(() => {
-          selectedMole.status = MOLE_STATUS.HIDDEN;
+          selectedCamera.status = CAMERA_STATUS.HIDDEN;
         }, randomHideSecs);
       }
     },
-    hitMoleHandler(mole) {
-      if (mole.status !== MOLE_STATUS.VISIBLE) return;
-      mole.status = MOLE_STATUS.HIT;
+    hitCameraHandler(camera) {
+      if (camera.status !== CAMERA_STATUS.VISIBLE) return;
+      camera.status = CAMERA_STATUS.HIT;
       this.score += 50;
       setTimeout(() => {
-        mole.status = MOLE_STATUS.HIDDEN;
+        camera.status = CAMERA_STATUS.HIDDEN;
       }, 200);
     },
     restartGame() {
-      this.restartCount++
-      this.scores.push(this.score)
-      this.score = 0
-      this.resetMoles()
-      this.startGame()
+      this.restartCount++;
+      this.timeRemainingSecs = 30;
+      this.score = 0;
+      this.resetCameras();
+      this.startGame();
     },
-    resetMoles() {
-      this.lines.forEach(({ moles }) => {
-        moles.forEach((mole) => {
-          mole.status = MOLE_STATUS.HIDDEN;
+    resetCameras() {
+      this.lines.forEach(({cameras}) => {
+        cameras.forEach((camera) => {
+          camera.status = CAMERA_STATUS.HIDDEN;
         });
       });
     },
     finishGame() {
-      this.scores.push(this.score)
-      const maxScore = Math.max(...this.scores)
-      this.$emit('next', maxScore)
-      this.resetMoles()
-      this.score = 0
-      this.scores = []
-    }
+      this.scores.push(this.score);
+      this.$emit('next', this.getHighestScore);
+      this.resetCameras();
+      this.score = 0;
+      this.scores = [];
+    },
   },
 };
 </script>
