56305d64eff16b9ea3a56ca7892432de09c3c157
oelleuch1 <elleuchoussama4@gmail.com>
2023-01-12 09:33:29 +0100

    fix cookie timeout

--- components/ui/cookie-modal.vue
+++ components/ui/cookie-modal.vue
@@ -64,7 +64,7 @@ export default {
         } else {
           this.$emit('input', true);
         }
-      }, 1000);
+      }, 2000);
     },
     acceptCookies() {
       localStorage.setItem(`privacy-game-cookie-accepted`, 'true');
